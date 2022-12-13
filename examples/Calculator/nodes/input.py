d433d95f8291bc851ee6702b30f6fc7c64695faa
Aasem Said <AasemTC@gmail.com>
2022-12-14 00:51:04 +0200

    The Node-Editor package after refactoring and re-engineering, ready to use

---
+++ examples/Calculator/nodes/input.py
@@ -0,0 +1,61 @@
+from qtpy.QtWidgets import QLineEdit
+from qtpy.QtCore import Qt
+from examples.Calculator.calc_conf import register_node, OP_NODE_INPUT
+from examples.Calculator.calc_node_base import CalcNode, CalcGraphicsNode
+from nodeeditor.base_nodes.func_content_widget import AllContentWidgetFunctions
+from nodeeditor.base_system_properties.utils_no_qt import dumpException
+
+
+class CalcInputContent(AllContentWidgetFunctions):
+    def createContentWidget(self):
+        self.edit = QLineEdit("1", self)
+        self.edit.setAlignment(Qt.AlignRight)
+        self.edit.setObjectName(self.node.content_label_objname)
+
+    def serialize(self):
+        res = super().serialize()
+        res['value'] = self.edit.text()
+        return res
+
+    def deserialize(self, data, hashmap={}):
+        res = super().deserialize(data, hashmap)
+        try:
+            value = data['value']
+            self.edit.setText(value)
+            return True & res
+        except Exception as e:
+            dumpException(e)
+        return res
+
+
+@register_node(OP_NODE_INPUT)
+class CalcNode_Input(CalcNode):
+    icon = "icons/in.png"
+    op_code = OP_NODE_INPUT
+    op_title = "Input"
+    content_label_objname = "calc_node_input"
+
+    def __init__(self, scene):
+        super().__init__(scene, inputs=[], outputs=[3])
+        self.eval()
+
+    def getInnerClasses(self):
+        self.content = CalcInputContent(self)
+        self.grNode = CalcGraphicsNode(self)
+        self.content.edit.textChanged.connect(self.onInputChanged)
+
+    def evalImplementation(self):
+        u_value = self.content.edit.text()
+        s_value = int(u_value)
+        self.value = s_value
+        self.markDirty(False)
+        self.markInvalid(False)
+
+        self.markDescendantsInvalid(False)
+        self.markDescendantsDirty()
+
+        self.grNode.setToolTip("")
+
+        self.evalChildren()
+
+        return self.value
