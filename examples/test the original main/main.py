b7ca9536d3efb2bbabd6b06dbb9dfe9371d8d09d
Nesrien Hassan <nesrienhassan83@gmail.com>
2023-02-01 20:51:34 +0200

    The General Form is Created

--- examples/test the original main/main.py
+++ examples/test the original main/main.py
@@ -1,7 +1,7 @@
 import os, sys, inspect
 from qtpy.QtWidgets import QApplication
 
-sys.path.insert(0, os.path.join( os.path.dirname(__file__), "..", ".." ))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
 
 from nodeeditor.base_system_properties.utils import loadStylesheet
 from nodeeditor.base_system_properties.home_window import NodeEditorWindow
@@ -12,8 +12,8 @@ if __name__ == '__main__':
 
     wnd = NodeEditorWindow()
     wnd.nodeeditor.addNodes()
-    module_path = os.path.dirname( inspect.getfile(wnd.__class__) )
+    module_path = os.path.dirname(inspect.getfile(wnd.__class__))
 
-    loadStylesheet( os.path.join( module_path, 'qss/nodestyle.qss') )
+    loadStylesheet(os.path.join(module_path,'qss/nodestyle.qss'))
 
     sys.exit(app.exec_())
