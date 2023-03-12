f49219bb261ca73f710962bd87e9510d8fdf20a0
Aasem Said <AasemTC@gmail.com>
2023-03-12 18:05:22 +0200

    the logic of the sort node is done

--- examples/DataScience/nodes/sort_df.py
+++ examples/DataScience/nodes/sort_df.py
@@ -1,16 +1,17 @@
 import pandas as pd
-import self as self
 from PyQt5.QtWidgets import QLabel, QComboBox
 from pandas.core.interchange import dataframe
 
 from examples.DataScience.datascience_conf import register_node, OP_NODE_SORT_DF
 from examples.DataScience.datascience_node_base import DataScienceNode, DataScienceGraphicalNode, DataScienceContent
 
+
 class DataScienceGraphicalSortDf(DataScienceGraphicalNode):
     def nodeSizes(self):
         super().nodeSizes()
         self.width = 210
         self.height = 130
+
 
 class DataScienceContentSortDf(DataScienceContent):
 
@@ -34,7 +35,7 @@ class DataScienceContentSortDf(DataScienceContent):
         column1 = self.combo_box.currentText()
         return column1
 
-    def know_the_change(self,index):
+    def know_the_change(self, index):
         old_item = self.combo_box.itemText(index - 1) if index > 0 else self.combo_box.itemText(0)
         new_item = self.combo_box.currentText()
 
@@ -56,13 +57,14 @@ class DataScienceNodeSortDf(DataScienceNode):
         super().__init__(scene, inputs, outputs)
 
     def getInnerClasses(self):
-            self.content = DataScienceContentSortDf(self)
-            self.grNode = DataScienceGraphicalSortDf(self)
-
+        self.content = DataScienceContentSortDf(self)
+        self.grNode = DataScienceGraphicalSortDf(self)
 
     def evaluationImplementation(self):
         super().evaluationImplementation()
         first_input = self.getInput(0)
+
+        dataframe = pd.DataFrame(first_input.value)
 
         if first_input is None:
             self.markInvalid()
@@ -73,7 +75,7 @@ class DataScienceNodeSortDf(DataScienceNode):
             val = self.evaluationOperation(first_input.nodeEvaluation())
             self.value = val
 
-            if val.empty:
+            if dataframe.empty:
                 self.grNode.setToolTip("Empty Data Frame")
                 self.markInvalid(True)
 
@@ -94,7 +96,6 @@ class DataScienceNodeSortDf(DataScienceNode):
                 return self.value
 
             elif self.content.know_the_change(self.content.combo_box.currentIndex()):
-
                 self.markReady(False)
                 self.markInvalid(False)
                 self.grNode.setToolTip("")
@@ -108,39 +109,41 @@ class DataScienceNodeSortDf(DataScienceNode):
 
     def evaluationOperation(self, input1, **kwargs):
 
-      dataframe  = pd.DataFrame(input1)
-      self.col_names = list(dataframe.columns)
+        dataframe = pd.DataFrame(input1)
 
-      if self.content.combo_box.count() == 0:
+        self.col_names = list(dataframe.columns)
 
-          self.content.combo_box.addItems(self.col_names)
+        if self.content.combo_box.count() == 0:
+            self.content.combo_box.addItems(self.col_names)
 
-      else:
-        pass
+        else:
+            pass
 
         chosen_col = self.content.printSelectedColumnsCombo1()
-        df=dataframe.sort_values(by=chosen_col,ascending=True)
-        return df 
+        df = dataframe.sort_values(by=chosen_col, ascending=True)
 
+        self.content.combo_box.currentTextChanged.connect(self.onStatuesChange)
 
+        return df
 
-#Sorting by a Column in Ascending Order
+        # Sorting by a Column in Ascending Order
+
     ##df=dataframe.sort_values(by=chosen_col,ascending=True)
-      #DataFrame sorted in descending order,
-#df =dataframe.sort_values(by=chosen_col,ascending=True)
+    # DataFrame sorted in descending order,
+    # df =dataframe.sort_values(by=chosen_col,ascending=True)
 
-     #Sorting by Multiple Columns in Ascending Order
-     ## dataframe.sort_values(by= chosen_col)
+    # Sorting by Multiple Columns in Ascending Order
+    ## dataframe.sort_values(by= chosen_col)
 
-      #Sorting by Index in Ascending Order
+    # Sorting by Index in Ascending Order
 
-     # sorted_df = dataframe.sort_values(by=["Open", "High"])
-#by assigned index w by5lehm homa bs ale ybano
+    # sorted_df = dataframe.sort_values(by=["Open", "High"])
+    # by assigned index w by5lehm homa bs ale ybano
 
     ##df = dataframe.set_index([chosen_col])
-  ##  df=dataframe.sort_index(axis=1)
-    #by index  assigned_index_
-   # df=dataframe.sort_index(chosen_col)
+    ##  df=dataframe.sort_index(axis=1)
+    # by index  assigned_index_
+    # df=dataframe.sort_index(chosen_col)
 
     def onStatuesChange(self):
         self.markReady()
@@ -155,8 +158,9 @@ class DataScienceNodeSortDf(DataScienceNode):
         if finput_port and foutput_port is not None:
             self.nodeEvaluation()
 
-        elif finput_port or foutput_port is None:
+        elif finput_port is None:
             self.markInvalid()
+            self.grNode.setToolTip("Connect input with dataframe")
 
         elif finput_port and foutput_port is None:
             self.markReady()
