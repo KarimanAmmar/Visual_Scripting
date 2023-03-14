82b3c22caded7d1166e4a5ed565e8aebc568c967
DESKTOP-FUD76O6\Kariman <kariman.mohmed@gmail.com>
2023-03-14 06:14:02 +0200

    pivot by name

--- examples/DataScience/nodes/Pivot By Value.py
+++ examples/DataScience/nodes/Pivot By Value.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from PyQt5.QtWidgets import QLabel, QComboBox
 
-from examples.DataScience.datascience_conf import register_node, OP_NODE_PIVOT
+from examples.DataScience.datascience_conf import register_node, OP_NODE_PIVOT_BY_VALUE
 from examples.DataScience.datascience_node_base import DataScienceNode, DataScienceGraphicalNode, DataScienceContent
 
 class DataScienceGraphicalPivot(DataScienceGraphicalNode):
     def nodeSizes(self):
         super().nodeSizes()
         self.width = 210
-        self.height = 180
+        self.height = 280
         self.edge_roundness = 6
         self.edge_padding = 0
         self.title_horizontal_padding = 8
@@ -44,10 +44,22 @@ class DataSciencePivotContent(DataScienceContent):
                                         "padding: 6px;")
 
 
+        self.lbl = QLabel("Rename Value Column : ", self)
+        self.lbl.move(30, 150)
+        self.lbl.setStyleSheet("font: bold 13px;")
+
+        self.combobox3 = QComboBox(self)
+        self.combobox3.move(30, 185)
+        self.combobox3.resize(150, 28)
+        self.combobox3.setStyleSheet("background-color: #5885AF;"
+                                       "border-radius: 10px;"
+                                       "font: bold 14px;"
+                                        "padding: 6px;")
+
 
         self.combobox1.currentIndexChanged.connect(self.know_the_changeCombo1)
         self.combobox2.currentIndexChanged.connect(self.know_the_changeCombo2)
-
+        self.combobox3.currentIndexChanged.connect(self.know_the_changeCombo3)
 
 
     def printSelectedColumnsCombo1(self):
@@ -57,6 +69,10 @@ class DataSciencePivotContent(DataScienceContent):
     def printSelectedColumnsCombo2(self):
         column2 = self.combobox2.currentText()
         return column2
+
+    def printSelectedColumnsCombo3(self):
+        column3 = self.combobox3.currentText()
+        return column3
 
     def know_the_changeCombo1(self, index):
         old_item = self.combobox1.itemText(index - 1) if index > 0 else self.combobox1.itemText(0)
@@ -76,14 +92,22 @@ class DataSciencePivotContent(DataScienceContent):
         else:
             return True
 
+    def know_the_changeCombo3(self, index):
+        old_item = self.combobox3.itemText(index - 1) if index > 0 else self.combobox3.itemText(0)
+        new_item = self.combobox3.currentText()
+
+        if old_item == new_item:
+            return False
+        else:
+            return True
 
 
 
-@register_node(OP_NODE_PIVOT)
+@register_node(OP_NODE_PIVOT_BY_VALUE)
 class DataScienceNodePivot(DataScienceNode):
     icon = "icons/slicing.png"
-    op_code = OP_NODE_PIVOT
-    op_title = "Pivot"
+    op_code = OP_NODE_PIVOT_BY_VALUE
+    op_title = "Pivot By Value"
 
 
     def __init__(self, scene, inputs=[3], outputs=[3]):
@@ -180,23 +204,27 @@ class DataScienceNodePivot(DataScienceNode):
 
             self.content.combobox1.addItems(self.col_names)
             self.content.combobox2.addItems(self.col_names)
-
+            self.content.combobox3.addItems(self.col_names)
 
         else:
             pass
 
         chosen_col1 = self.content.printSelectedColumnsCombo1()
         chosen_col2 = self.content.printSelectedColumnsCombo2()
-
+        chosen_col3 = self.content.printSelectedColumnsCombo3()
 
         index1 = dataframe.columns.get_loc(chosen_col1)
         index2 = dataframe.columns.get_loc(chosen_col2)
+        index3 = dataframe.columns.get_loc(chosen_col3)
 
-        df_Pivot = dataframe.pivot(index=dataframe.columns[index1], columns=dataframe.columns[index2])
+
+        df_Pivot = dataframe.pivot(index=dataframe.columns[index1], columns=dataframe.columns[index2] , values= dataframe.columns[index3])
 
         self.content.combobox1.currentIndexChanged.connect(self.onStatuesChange)
         self.content.combobox2.currentIndexChanged.connect(self.onStatuesChange)
+        self.content.combobox3.currentIndexChanged.connect(self.onStatuesChange)
 
+        print(index3)
         print(index2)
         print(index1)
         print(df_Pivot)
@@ -212,7 +240,7 @@ class DataScienceNodePivot(DataScienceNode):
 
         self.content.combobox1.clear()
         self.content.combobox2.clear()
-
+        self.content.combobox3.clear()
         self.markReady()
 
         if finput_port and foutput_port is not None:
@@ -223,3 +251,5 @@ class DataScienceNodePivot(DataScienceNode):
 
         elif finput_port and foutput_port is None:
             self.markReady()
+
+
